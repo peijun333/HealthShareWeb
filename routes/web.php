1e56c7c6ca432523d0c87ed55b5db75d1329bab1
peijun333 <Kzyun0205>
2018-12-03 21:29:50 +0900

    delete header
    headerの取り除き

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 Route::post('register/pre_check', 'Auth\RegisterController@pre_check')->name('register.pre_check');
 
 //本会員登録入力
